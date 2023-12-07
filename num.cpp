cf26c1b021568d9dfa2892f63dbccec77f909169
Alexis <90709447+AlexisMaster131@users.noreply.github.com>
2023-12-07 14:31:20 -0600

    Add files via upload

---
+++ num.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+    //Entraada y salida normal de datos
+    int a, b;
+    string x;
+
+    cin >>a >> b >> x;
+    cout << a<<< " " << b << " " << x << endl;
+
+    //usando funciones de c
+    int c, d;
+    scanf("%d %d", &c, &d);
+    printf("La variable 1 es %d y la variable 2 es %d \n")
+}
